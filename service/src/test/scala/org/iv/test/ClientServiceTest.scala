6c4b8b2bdb561fe09fa3b5ea4dffabacb2df4450
twr143 <twr143@gmail.com>
2021-07-06 11:11:35 +0300

    service testing, 2

--- service/src/test/scala/org/iv/test/ClientServiceTest.scala
+++ service/src/test/scala/org/iv/test/ClientServiceTest.scala
@@ -10,40 +10,57 @@ import akka.http.scaladsl.model._
 import org.iv.JsonSupport._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.should.Matchers
-import akka.http.scaladsl.server._
+import akka.http.scaladsl.model.StatusCodes._
 import org.iv.EmpServer.route
+import org.iv.model.Employee
 
 import scala.concurrent.Future
 
 /**
  * Created by twr143 on 05.07.2021 at 13:54.
  */
-class ClientServiceTest extends AnyWordSpec with MockFactory with Matchers with ScalatestRouteTest with BeforeAndAfterAll with ScalaFutures{
-  //  val mock: HttpExt = mock[HttpExt]
-  //  (mock.singleRequest _).expects(,
-  //    "", "create"),*,*,*)
-  //    .returning(Future.successful(HttpResponse(entity = HttpEntity(ContentTypes.`application/json`, CreateResponse("created").toJson.toString))))
-  //  val service = new EmpService(mock, "http://localhost:8081/")
+class ClientServiceTest extends AnyWordSpec with MockFactory with Matchers with ScalatestRouteTest with BeforeAndAfterAll with ScalaFutures {
   implicit val mockSendRequest = mockFunction[HttpRequest, Future[HttpResponse]]
-  val service = new EmpService(Http(),"http://localhost:8081/")
-    mockSendRequest.expects(*).onCall({req: HttpRequest =>
-          if (req.method == HttpMethods.POST && req.uri.toString.endsWith("/create")) {
-            Future.successful(HttpResponse(StatusCodes.OK, entity = HttpEntity(ContentTypes.`application/json`, CreateResponse("created").toJson.toString)))
-          } else {
-            Future.successful(HttpResponse(StatusCodes.BadRequest, entity = HttpEntity("bad request")))
-          }
-        })
+  val service = new EmpService(Http(), "http://localhost:8081/")
 
- val r = route(service)
+
+  val r = route(service)
 
   "client layer" should {
-    "create first employee" in {
-        Post("/create", EmployeeJson("igor", "2021-07-05T03:12:13")) ~> r ~> check {
-          responseAs[CreateResponse].result shouldEqual "created"
-        }
+    "create employee" in {
+      val resp = HttpResponse(OK, entity = HttpEntity(ContentTypes.`application/json`, CreateResponse("created").toJson.toString))
+      mockSendRequest.expects(where { r: HttpRequest => r.uri.toString.endsWith("create") }).onCall({ _ => Future.successful(resp) })
+      Post("/create", EmployeeJson("igor", "2021-07-05T03:12:13")) ~> r ~> check {
+        responseAs[CreateResponse].result shouldEqual "created"
       }
-
     }
+    "return validation error if create employee doesn't pass validation" in {
+      Post("/create", EmployeeJson("i", "2021-07-05T03:12:13")) ~> r ~> check {
+        response.status shouldEqual BadRequest
+      }
+    }
+    "list employees by query" in {
+      val resp = HttpResponse(OK, entity = HttpEntity(ContentTypes.`application/json`, QueryResponse(List(Employee("", ""))).toJson.toString))
+      mockSendRequest.expects(where { r: HttpRequest => r.uri.toString.endsWith("query") }).onCall({ _ => Future.successful(resp) })
+      Post("/query", QueryJson("name:*")) ~> r ~> check {
+        responseAs[QueryResponse].records.size shouldEqual 1
+      }
+    }
+    "update by query" in {
+      val resp = HttpResponse(OK, entity = HttpEntity(ContentTypes.`application/json`, UpdateDeleteResponse(2).toJson.toString))
+      mockSendRequest.expects(where { r: HttpRequest => r.uri.toString.endsWith("update") }).onCall({ _ => Future.successful(resp) })
+      Post("/update", UpdateJson("name:*", "")) ~> r ~> check {
+        responseAs[UpdateDeleteResponse].numRecords shouldEqual 2
+      }
+    }
+    "delete by query" in {
+      val resp = HttpResponse(OK, entity = HttpEntity(ContentTypes.`application/json`, UpdateDeleteResponse(2).toJson.toString))
+      mockSendRequest.expects(where { r: HttpRequest => r.uri.toString.endsWith("delete") }).onCall(_ => Future.successful(resp))
+      Post("/delete", DeleteJson("name:*")) ~> r ~> check {
+        responseAs[UpdateDeleteResponse].numRecords shouldEqual 2
+      }
+    }
+  }
 
 
 }
